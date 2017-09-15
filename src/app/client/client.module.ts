59a6865e84e6b8314ccafe367cbd4cdb14484fdc
lijiang <lijiang@gogotrans.com>
2017-09-15 20:17:58 +0700

    添加客户端模块

---
+++ src/app/client/client.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ClientRoutingModule } from './client-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ClientRoutingModule
+  ],
+  declarations: []
+})
+export class ClientModule { }
